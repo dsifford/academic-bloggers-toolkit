076672d90522c3e5eff6d5d2f032368ae8193f4a
dsifford <dereksifford@gmail.com>
2015-06-20 00:36:00 -0400

    Reformat menu + Add linkout to feature request trello board

--- inc/tinymce-buttons.js
+++ inc/tinymce-buttons.js
@@ -65,13 +65,24 @@
 																		value: ''
 																	},
 																	{
+																		type: 'checkbox',
+																		name: 'ref_id_include_link',
+																		label: 'Include link to PubMed?'
+																	},
+																	{
 																		type: 'label',
 																		text: 'Note: DOI method rarely works. Use PMID/PMCID whenever possible.'
 												                    }
 												                  ],
 															onsubmit: function(e) {
+																if ( e.data.ref_id_include_link ) { 
+																	var linkLogic = ' link=' + e.data.ref_id_include_link + ']';
+																} else { 
+																	var linkLogic = ']';
+																}
+
 																editor.insertContent(
-																	'[ref id=&quot;' + e.data.ref_id_number + '&quot;]'
+																	'[ref id=&quot;' + e.data.ref_id_number + '&quot;' + linkLogic
 																);
 															}
 														});
@@ -79,18 +90,37 @@
 											}
 										]
 							},
+							{text: '-'},
 							{
-								text: 'Peer Review Tools',
-								menu:   [
-											// TODO: Peer Review Box (Disabled)
-											{
-												text: 'Insert Peer Review Box',
-												disabled: true
+								text: 'Request More Tools',
+								onclick: function() {
+									editor.windowManager.open(
+										{
+											title: 'Request More Tools',
+											body: [
+
+												{
+													type: 'label',
+													text: "Have a feature or tool in mind that isn't available? Visit the link below to send a feature request. We'll do our best to make it happen."
+												},
+												{
+													type: 'button',
+													text: 'Send us your thoughts!',
+													onclick : function() {
+													                    window.open('https://trello.com/b/nFxfo6iO/academic-blogger-s-toolkit','_blank');
+													                },
+												}
+
+											],
+											onsubit: function() {
+												return;
 											}
-										]
-							}
-						]
-			}
+										});
+								}}
+
+							]
+						}
+			);}
 		);
-	});
-})();
+	})
+();
