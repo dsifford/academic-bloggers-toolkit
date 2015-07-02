cb8a92a0a0e9f0ca69ac40ec050b63734df1056a
dsifford <dereksifford@gmail.com>
2015-07-01 23:01:01 -0400

    Update README

--- academic-bloggers-toolkit.php
+++ academic-bloggers-toolkit.php
@@ -2,7 +2,7 @@
 
 /*
  *	Plugin Name: Academic Blogger's Toolkit
- *  Plugin URI: null
+ *	Plugin URI: null
  *	Description: A Wordpress plugin extending the functionality of Wordpress for Academic Blogging
  *	Version: 1.0
  *	Author: Derek P Sifford
