38a1c06e5f462b8d461df4b02f790ed838f23845
Derek P Sifford <dereksifford@gmail.com>
2016-11-15 21:00:23 -0500

    chore: build version 4.7.2

--- src/academic-bloggers-toolkit.php
+++ src/academic-bloggers-toolkit.php
@@ -4,14 +4,14 @@
  *	Plugin Name: Academic Blogger's Toolkit
  *	Plugin URI: https://wordpress.org/plugins/academic-bloggers-toolkit/
  *	Description: A plugin extending the functionality of Wordpress for academic blogging
- *	Version: 4.7.1
+ *	Version: 4.7.2
  *	Author: Derek P Sifford
  *	Author URI: https://github.com/dsifford
  *	License: GPL3 or later
  *  Text Domain: academic-bloggers-toolkit
  */
 
-define('ABT_VERSION', '4.7.1');
+define('ABT_VERSION', '4.7.2');
 
 /**
  * Load plugin translations
