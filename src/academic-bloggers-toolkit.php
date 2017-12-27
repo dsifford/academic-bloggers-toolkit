905082a40d6620519d515eef5eca203d6ea94775
Derek P Sifford <dereksifford@gmail.com>
2017-12-27 13:51:38 -0500

    bump version

--- src/academic-bloggers-toolkit.php
+++ src/academic-bloggers-toolkit.php
@@ -3,7 +3,7 @@
  *	Plugin Name: Academic Blogger's Toolkit
  *	Plugin URI: https://wordpress.org/plugins/academic-bloggers-toolkit/
  *	Description: A plugin extending the functionality of Wordpress for academic blogging
- *	Version: 4.12.0
+ *	Version: 4.12.1
  *	Author: Derek P Sifford
  *	Author URI: https://github.com/dsifford
  *  Text Domain: academic-bloggers-toolkit
