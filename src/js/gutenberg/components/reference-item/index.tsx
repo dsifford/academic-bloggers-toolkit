efa7c862508602155fc380a62850849e7c48b2d4
Derek Sifford <dereksifford@gmail.com>
2019-05-01 18:21:51 -0400

    perf: memoize ReferenceItem renders

--- src/js/gutenberg/components/reference-item/index.tsx
+++ src/js/gutenberg/components/reference-item/index.tsx
@@ -1,38 +1,55 @@
+import { useMemo } from '@wordpress/element';
 import { _x } from '@wordpress/i18n';
+import DOMPurify from 'dompurify';
 
 import { CSLDate, CSLPerson } from 'utils/csl';
 import { firstTruthyValue } from 'utils/data';
 
 import styles from './style.scss';
 
-export default function ReferenceItem(item: CSL.Data) {
+interface Props {
+    item: CSL.Data;
+}
+
+export default function ReferenceItem({ item }: Props) {
+    const title = useMemo(
+        () => ({ __html: DOMPurify.sanitize(item.title || '') }),
+        [item.title],
+    );
+    const authors = useMemo(() => CSLPerson.getNames(item.author, 3), [
+        item.author,
+    ]);
+    const publication = useMemo(
+        () =>
+            firstTruthyValue(
+                item,
+                [
+                    'journalAbbreviation',
+                    'container-title-short',
+                    'container-title',
+                    'publisher',
+                ],
+                _x(
+                    'n.p.',
+                    'Abbreviation for "no publisher"',
+                    'academic-bloggers-toolkit',
+                ),
+            ),
+        [
+            item.journalAbbreviation,
+            item['container-title-short'],
+            item['container-title'],
+            item.publisher,
+        ],
+    );
+    const year = useMemo(() => CSLDate.getYear(item.issued), [item.issued]);
     return (
         <>
-            <strong>{item.title}</strong>
-            {item.author && item.author.length > 0 && (
-                <div className={styles.authors}>
-                    {CSLPerson.getNames(item.author, 3)}
-                </div>
-            )}
-            <div>
-                <i>
-                    {firstTruthyValue(
-                        item,
-                        [
-                            'journalAbbreviation',
-                            'container-title-short',
-                            'container-title',
-                            'publisher',
-                        ],
-
-                        _x(
-                            'n.p.',
-                            'Abbreviation for "no publisher"',
-                            'academic-bloggers-toolkit',
-                        ),
-                    )}
-                </i>
-                <span>{CSLDate.getYear(item.issued)}</span>
+            <strong dangerouslySetInnerHTML={title} />
+            {authors && <div className={styles.authors}>{authors}</div>}
+            <div className={styles.meta}>
+                <i>{publication}</i>
+                <span>({year})</span>
             </div>
         </>
     );
